020d9500340976f2339fa6af8e92a7a77de0f029
hancel.lin <imlinhanchao@foxmail.com>
2020-07-24 15:21:06 +0800

    修正头像权限控制

--- routes/access.js
+++ routes/access.js
@@ -7,7 +7,7 @@ const Account = modules.account;
 router.all('/:interface/:fn*', function (req, res, next) {
     // 允许不登录访问的接口，若所有函数都允许，则写为 interface: '*'
     const no_login_interface = {
-        account: ['login', 'query', 'exist', 'create', 'exists'],
+        account: ['login', 'query', 'exist', 'create', 'exists', 'avatar'],
         lib: ['captcha'],
         snippet: ['query', 'get']
     };
