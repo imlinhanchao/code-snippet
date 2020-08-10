6ae4b0379fccf6bc181f8deb83f6916317f522f5
hancel.lin <imlinhanchao@foxmail.com>
2020-08-10 14:40:20 +0800

    分離評論框

---
+++ frontend/components/comment.vue
@@ -0,0 +1,149 @@
+<template>
+    <section class="comment-textarea">
+        <p class="comment-reply" v-if="floor > 0">
+            Reply <a :href="`#comment${floor}`">#{{floor+1}}</a>
+        </p>
+        <mavon-editor ref="md" v-model="comment.content"
+            language="en" :subfield="false"
+            toolbarsBackground="#1a1c1e"
+            previewBackground="#232323"
+            placeholder="Leave a comment"
+            :toolbars="toolbars"
+            codeStyle="railscasts"
+            :externalLink="$markdown.externalLink"
+            :autofocus="autofocus"
+        ></mavon-editor>
+        <p class="comment-submit">
+            <Button v-if="cancelable" @click="$emit('cancel')">Cancel</Button>
+            <Button type="success" @click="OnComment">Comment</Button>
+        </p>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Comment',
+    props: {
+        comment: {
+            type: Object,
+            required: true
+        },
+        autofocus: {
+            type: Boolean,
+            default: false
+        },
+        floor: {
+            type: Number,
+            default: -1
+        },
+        cancelable: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
+        return {
+            toolbars: {
+                bold: true, // 粗体
+                italic: true, // 斜体
+                header: true, // 标题
+                underline: true, // 下划线
+                strikethrough: true, // 中划线
+                mark: true, // 标记
+                superscript: true, // 上角标
+                subscript: true, // 下角标
+                quote: true, // 引用
+                ol: true, // 有序列表
+                ul: true, // 无序列表
+                link: true, // 链接
+                code: true, // code
+                table: true, // 表格
+                undo: true, // 上一步
+                redo: true, // 下一步
+                trash: true, // 清空
+                alignleft: true, // 左对齐
+                aligncenter: true, // 居中
+                alignright: true, // 右对齐
+                preview: true, // 预览
+            }
+        }
+    },
+    computed: {
+        
+    },
+    methods: {
+        async OnComment() {
+            try {
+                let rsp = await this.$store.dispatch(this.comment.id ? 'comment/update' : 'comment/create', this.comment);
+                if (rsp.state != 0) return this.$root.message($m.ERROR, rsp.msg);
+                let comment = rsp.data;
+                this.$emit('ok', comment);
+            } catch (error) {
+                console.error(error.message);
+                this.$root.message($m.ERROR, error.message);
+            }
+        },
+    }
+}
+</script>
+<style lang="less" scoped>
+.comment-textarea {
+    padding: 1em;
+    .comment-reply {
+        padding: 0 1em 1em;
+        font-style: italic;
+        a {
+            border-bottom: 1px dashed #FFF;
+        }
+    }
+    .comment-submit {
+        text-align: right;
+        margin-top:1em;
+    }
+}
+</style>
+<style lang="less">
+.comment-textarea {
+    .v-note-wrapper {
+        background: #232323;
+    }
+    .content-input-wrapper, .auto-textarea-wrapper, textarea {
+        background-color: transparent!important;
+        color: #e3e5e8
+    }
+    .v-note-wrapper .v-note-op {
+        border-bottom-color: #373c3e;
+    }
+    .auto-textarea-wrapper .auto-textarea-input {
+        color: inherit;
+    }
+    .add-image-link-wrapper {
+        div.add-image-link {
+            background: #131516;
+            border-radius: 6px;
+            width: 80%;
+            left: 0;
+            right: 0;
+            margin: auto;
+            padding: 1em;
+            .op-btn {
+                margin-top: 1em;
+                &.op-btn.cancel:hover {
+                    color: #e3e5e8;
+                }
+            }
+            .input-wrapper {
+                border-color: #373c3e;
+                width: 90%;
+                margin: 1em auto;
+            }
+            input {
+                background: transparent;
+            }
+            i {
+                color: #373c3e;
+            }
+        }
+    }
+}
+</style>
--- frontend/views/snippet.vue
+++ frontend/views/snippet.vue
@@ -113,24 +113,8 @@
                             <Icon v-if="!loading" custom="fa fa-ellipsis-h"></Icon>
                             <i v-if="loading" class="fa fa-spinner fa-pulse fa-2x fa-fw"></i>
                         </section>
-                        <section class="comment-textarea" v-show="$root.isLogin">
-                            <p class="comment-reply" v-if="replyfloor !== null">
-                                Reply <a :href="`#comment${replyfloor}`">#{{replyfloor+1}}</a>
-                            </p>
-                            <mavon-editor ref="md" v-model="comment.content"
-                                language="en" :subfield="false"
-                                toolbarsBackground="#1a1c1e"
-                                previewBackground="#232323"
-                                placeholder="Leave a comment"
-                                :toolbars="toolbars"
-                                codeStyle="railscasts"
-                                :externalLink="$markdown.externalLink"
-                                :autofocus="$win.location.hash=='#comment'"
-                            ></mavon-editor>
-                            <p class="comment-submit">
-                                <Button type="success" @click="OnComment">Comment</Button>
-                            </p>
-                        </section>
+                        <Comment :comment="comment" v-if="$root.isLogin" :floor="getReplyFloor(comment.reply)"
+                            :autofocus="$win.location.hash=='#comment'" @ok="OnComment"></Comment>
                     </article>
                 </TabPane>
                 <TabPane :label="getLabel('Stars', stars.length, { type: 'md-star-outline' })" name="star">
@@ -185,7 +169,8 @@ export default {
     components: {
         Action: () => import('../components/action'), 
         Execute: () => import('../components/execute'),
-        Preview: () => import('../components/preview')
+        Preview: () => import('../components/preview'),
+        Comment: () => import('../components/comment')
     },
     async mounted() {
         if(!this.id) {
@@ -231,30 +216,6 @@ export default {
                 content: '',
                 reply: '',
                 snippet: ''
-            },
-            toolbars: {
-                bold: true, // 粗体
-                italic: true, // 斜体
-                header: true, // 标题
-                underline: true, // 下划线
-                strikethrough: true, // 中划线
-                mark: true, // 标记
-                superscript: true, // 上角标
-                subscript: true, // 下角标
-                quote: true, // 引用
-                ol: true, // 有序列表
-                ul: true, // 无序列表
-                link: true, // 链接
-                code: true, // code
-                table: true, // 表格
-                help: true, // 帮助
-                undo: true, // 上一步
-                redo: true, // 下一步
-                trash: true, // 清空
-                alignleft: true, // 左对齐
-                aligncenter: true, // 居中
-                alignright: true, // 右对齐
-                preview: true, // 预览
             }
         };
     },
@@ -308,22 +269,18 @@ export default {
         OnPage(type, page) {
             this.$router.push(`/s/${this.snippet.id}/${type}/${page}`);
         },
-        async OnComment() {
-            try {
-                let rsp = await this.$store.dispatch('comment/create', this.comment);
-                if (rsp.state != 0) return this.$root.message($m.ERROR, rsp.msg);
-                let comment = rsp.data;
-                comment.index = this.comments.length;
-                if(comment.reply) comment.floor = this.comments.find(r => r.id == c.reply).index;
-                comment.user = this.$root.loginUser;
-                comment.create_time = parseInt(comment.create_time)
-                this.comments.push(comment);
-                this.comment.reply = '';
-                this.comment.content = '';
-            } catch (error) {
-                console.error(error.message);
-                this.$root.message($m.ERROR, error.message);
-            }
+        OnComment(comment) {
+            comment.index = this.comments.length;
+            if(comment.reply) comment.floor = this.comments.find(r => r.id == c.reply).index;
+            comment.user = this.$root.loginUser;
+            comment.create_time = parseInt(comment.create_time)
+            this.comments.push(comment);
+            this.comment.reply = '';
+            this.comment.content = '';
+        },
+        getReplyFloor(reply) {
+            let comment = this.comments.find(c => c.id == reply);
+            return comment ? comment.index : -1;
         },
         getLabel(title, count, { custom, type }) {
             return (h) => {
@@ -414,10 +371,13 @@ export default {
 
                 if (rsp && rsp.state == 0) {
                     let users = rsp.data.data;
+                    let ids = this.comments.map(c => c.id);
                     comments.forEach((c, i) => {
                         c.user = users.find(u => u.username == c.username);
                         c.index = i;
                         if (c.reply) c.floor = comments.slice(0, i).find(r => r.id == c.reply).index;
+                        let index = ids.indexOf(c.id);
+                        if (index >= 0) this.comments.splice(index, 1);
                         this.comments.push(c)
                     });
                     this.page.comment++;
@@ -498,7 +458,7 @@ export default {
         },
         replyfloor() {
             let comment = this.comments.find(c => c.id == this.comment.reply);
-            return comment ? comment.index : null;
+            return comment ? comment.index : -1;
         }
     },
     watch: {
@@ -766,20 +726,7 @@ export default {
         background: #24292e94;
     }
 }
-.comment-textarea {
-    padding: 1em;
-    .comment-reply {
-        padding: 0 1em 1em;
-        font-style: italic;
-        a {
-            border-bottom: 1px dashed #FFF;
-        }
-    }
-    .comment-submit {
-        text-align: right;
-        margin-top:1em;
-    }
-}
+
 @media (max-width: 480px) {
     .header {
         position: relative;
@@ -854,47 +801,5 @@ export default {
         padding: .5em 1em;
     }
 }
-.comment-textarea {
-    .v-note-wrapper {
-        background: #232323;
-    }
-    .content-input-wrapper, .auto-textarea-wrapper, textarea {
-        background-color: transparent!important;
-        color: #e3e5e8
-    }
-    .v-note-wrapper .v-note-op {
-        border-bottom-color: #373c3e;
-    }
-    .auto-textarea-wrapper .auto-textarea-input {
-        color: inherit;
-    }
-    .add-image-link-wrapper {
-        div.add-image-link {
-            background: #131516;
-            border-radius: 6px;
-            width: 80%;
-            left: 0;
-            right: 0;
-            margin: auto;
-            padding: 1em;
-            .op-btn {
-                margin-top: 1em;
-                &.op-btn.cancel:hover {
-                    color: #e3e5e8;
-                }
-            }
-            .input-wrapper {
-                border-color: #373c3e;
-                width: 90%;
-                margin: 1em auto;
-            }
-            input {
-                background: transparent;
-            }
-            i {
-                color: #373c3e;
-            }
-        }
-    }
-}
+
 </style>
