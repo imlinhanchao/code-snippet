0dd9a67beaf4aa6ae62a7b3162582466d4ef3834
Hancel.Lin <imlinhanchao@gmail.com>
2020-08-27 20:14:46 +0800

    更新 Action 组件多语言

--- frontend/components/action.vue
+++ frontend/components/action.vue
@@ -2,24 +2,24 @@
     <section class="action">
         <a v-if="$root.getCodeExt(code.filename) == 'html'" @click.prevent="previewModal=true" 
         class="action-link" :href="`${$config.base.preview_url}/view/${snippet.id}/${code.filename}`"
-        target="_blank" title="Preview">
+        target="_blank" :title="$t('preview')">
             <Icon custom="fa fa-chrome"></Icon>
         </a>
         <a v-if="render" :class="{ ischeck: source }"
-            title="View Source" @click="OnSource" 
+            :title="$t('view_src')" @click="OnSource" 
             class="action-link" >
             <Icon custom="fa fa-code"></Icon>
         </a>
-        <a title="Copy Code to Clipboard" class="action-link" 
+        <a :title="$t('copy_clipboard')" class="action-link" 
             v-clipboard:copy="code.content"
             v-clipboard:success="onCopy"
             v-clipboard:error="onError">
             <Icon custom="fa fa-files-o"></Icon>
         </a>
-        <a title="Download" @click="OnSave" class="action-link" >
+        <a :title="$t('download')" @click="OnSave" class="action-link" >
             <Icon custom="fa fa-download"></Icon>
         </a>
-        <Icon class="action-link execute" custom="fa fa-terminal" v-if="code.execute" title="Execute" @click="executeModal = true"></Icon>
+        <Icon class="action-link execute" custom="fa fa-terminal" v-if="code.execute" :title="$t('execute')" @click="executeModal = true"></Icon>
         <Execute v-if="executeModal" v-model="executeModal" :snippet="code" :codes="[code]" :auto="false"></Execute>
         <Preview v-if="previewModal" v-model="previewModal" :snippet="snippet" :code="code.filename"></Preview>
     </section>
@@ -56,10 +56,10 @@ export default {
     },
     methods: {
         onCopy() {
-            this.$Message.success('The code has been copied to your clipboard!');
+            this.$Message.success(this.$t('copied'));
         },
         onError(e) {
-            this.$Message.error('Copy Failed:' + e.message);
+            this.$Message.error(this.$t('copy_failed') + e.message);
         },
         OnSave() {
             saveAs(this.code.content, this.code.filename);
